5091ae0215e71b3f8793ec4fed416c619fbc1094
Sergey <diogersa@yandex.ru>
2020-03-07 17:44:40 +0300

    Это наш первый коммит

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let money = prompt("Ваш бюджет?"),
+    shop = prompt("Название вашего магазина?");
+obj mainList {
+    budget: "Бюджет",
+    shopName: "Имя магазина",
+    let 
+}
---
+++ js/main.js
@@ -0,0 +1,45 @@
+let number = 5;
+let string = "Hello";
+let sym = Symbol();
+let boolean = true;
+null;
+undefined;
+
+// massive
+obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+console.log(obj["name"]);
+
+let arr = ["plum", "orange", "apple"];
+console.log(arr[2]);
+
+// alert
+// alert("Hello!");
+// confirm("Are you here?");
+
+// let answer = +prompt("Есть ли вам 18 лет?", "18");
+
+// console.log( typeof(answer));
+
+// console.log("arr" + "-object");
+// console.log(4 + "-object");
+
+var incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(5 % 2);
+
+console.log("10" === 10);
+
+let isChecked = true;
+let isClose = true;
+
+console.log(isChecked || !isClose);
